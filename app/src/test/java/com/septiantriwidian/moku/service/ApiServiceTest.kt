31db125225fe7c9218755da35dbc7b08b274bd17
Septian-tri <Septiantriwidian@gmail.com>
2023-05-20 05:05:03 +0700

    Init Unit Testing

---
+++ app/src/test/java/com/septiantriwidian/moku/service/ApiServiceTest.kt
@@ -0,0 +1,17 @@
+package com.septiantriwidian.moku.service
+
+import junit.framework.JUnit4TestAdapter
+import org.junit.Assert.*
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class ApiServiceTest {
+
+    @Test
+    fun fetchImage() {
+        assertEquals(23, (20+2))
+    }
+}
